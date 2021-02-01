91dbda4172ef41cc8c79d1112679d8e2085745d0
Josh Stevens <joshstevens19@hotmail.co.uk>
2021-02-01 16:23:31 +0000

    remove unused contract tokens on networks

--- src/common/tokens/dai.ts
+++ src/common/tokens/dai.ts
@@ -14,62 +14,14 @@ export class DAI {
     };
   }
 
-  public static ROPSTEN() {
-    return {
-      chainId: ChainId.ROPSTEN,
-      contractAddress: '0xc778417E063141139Fce010982780140Aa0cD5Ab',
-      decimals: 18,
-      symbol: 'DAI',
-      name: 'Dai Stablecoin',
-    };
-  }
-
-  public static RINKEBY() {
-    return {
-      chainId: ChainId.RINKEBY,
-      contractAddress: '0xc778417E063141139Fce010982780140Aa0cD5Ab',
-      decimals: 18,
-      symbol: 'DAI',
-      name: 'Dai Stablecoin',
-    };
-  }
-
-  public static GORLI() {
-    return {
-      chainId: ChainId.GÖRLI,
-      contractAddress: '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
-      decimals: 18,
-      symbol: 'DAI',
-      name: 'Dai Stablecoin',
-    };
-  }
-
-  public static KOVAN() {
-    return {
-      chainId: ChainId.KOVAN,
-      contractAddress: '0xd0A1E359811322d97991E03f863a0C30C2cF029C',
-      decimals: 18,
-      symbol: 'DAI',
-      name: 'Dai Stablecoin',
-    };
-  }
-
   /**
-   * Get WETH token info by chain id
+   * Get DAI token info by chain id
    * @param chainId The chain id
    */
-  public static token(chainId: ChainId) {
+  public static token(chainId: ChainId | number) {
     switch (chainId) {
       case ChainId.MAINNET:
         return this.MAINNET();
-      case ChainId.ROPSTEN:
-        return this.ROPSTEN();
-      case ChainId.RINKEBY:
-        return this.RINKEBY();
-      case ChainId.GÖRLI:
-        return this.GORLI();
-      case ChainId.KOVAN:
-        return this.KOVAN();
       default:
         throw new Error(`${chainId} is not allowed`);
     }
--- src/common/tokens/usdc.ts
+++ src/common/tokens/usdc.ts
@@ -14,62 +14,14 @@ export class USDC {
     };
   }
 
-  public static ROPSTEN() {
-    return {
-      chainId: ChainId.ROPSTEN,
-      contractAddress: '0xc778417E063141139Fce010982780140Aa0cD5Ab',
-      decimals: 18,
-      symbol: 'USDC',
-      name: 'USD Coin',
-    };
-  }
-
-  public static RINKEBY() {
-    return {
-      chainId: ChainId.RINKEBY,
-      contractAddress: '0xc778417E063141139Fce010982780140Aa0cD5Ab',
-      decimals: 18,
-      symbol: 'USDC',
-      name: 'USD Coin',
-    };
-  }
-
-  public static GORLI() {
-    return {
-      chainId: ChainId.GÖRLI,
-      contractAddress: '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
-      decimals: 18,
-      symbol: 'USDC',
-      name: 'USD Coin',
-    };
-  }
-
-  public static KOVAN() {
-    return {
-      chainId: ChainId.KOVAN,
-      contractAddress: '0xd0A1E359811322d97991E03f863a0C30C2cF029C',
-      decimals: 18,
-      symbol: 'USDC',
-      name: 'USD Coin',
-    };
-  }
-
   /**
-   * Get WETH token info by chain id
+   * Get USDC token info by chain id
    * @param chainId The chain id
    */
-  public static token(chainId: ChainId) {
+  public static token(chainId: ChainId | number) {
     switch (chainId) {
       case ChainId.MAINNET:
         return this.MAINNET();
-      case ChainId.ROPSTEN:
-        return this.ROPSTEN();
-      case ChainId.RINKEBY:
-        return this.RINKEBY();
-      case ChainId.GÖRLI:
-        return this.GORLI();
-      case ChainId.KOVAN:
-        return this.KOVAN();
       default:
         throw new Error(`${chainId} is not allowed`);
     }
