bf2ce1924c5dd989c0f7285a377de05b54698f14
Josh Stevens <joshstevens19@hotmail.co.uk>
2021-02-01 13:07:38 +0000

    add a time on my playground

--- src/__tests__/playground.spec.ts
+++ src/__tests__/playground.spec.ts
@@ -3,6 +3,7 @@ import { UniswapPairSettings } from '../factories/pair/models/uniswap-pair-setti
 import { UniswapPair } from '../factories/pair/uniswap-pair';
 
 const routeTest = async () => {
+  console.log(new Date().getTime());
   const fromTokenContractAddress = '0x419D0d8BdD9aF5e606Ae2232ed285Aff190E711b';
   const toTokenContractAddress = '0x1985365e9f78359a9B6AD760e32412f4a445E862';
   const ethereumAddress = '0xB1E6079212888f0bE0cf55874B2EB9d7a5e02cD9';
@@ -24,7 +25,9 @@ const routeTest = async () => {
   const uniswapPairFactory = await uniswapPair.createFactory();
 
   const trade = await uniswapPairFactory.trade('10');
-  console.log(trade);
+  //console.log(trade);
+
+  console.log(new Date().getTime());
 
   process.stdin.resume();
 
