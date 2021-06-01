7e357cef280848e2bf14d19391e69c954bc31052
Josh Stevens <joshstevens19@hotmail.co.uk>
2021-06-01 13:51:23 +0100

    delete unused contracts

--- src/factories/uniswap-factory/v3/uniswap-contract.factory.v3.ts
+++ src/factories/uniswap-factory/v3/uniswap-contract.factory.v3.ts
@@ -4,10 +4,11 @@ import { EthersProvider } from '../../../ethers-provider';
 import { UniswapContractContextV3 } from '../../../uniswap-contract-context/uniswap-contract-context-v3';
 
 export class UniswapContractFactoryV3 {
-  private _uniswapFactoryContract = this._ethersProvider.getContract<FactoryContractContext>(
-    JSON.stringify(UniswapContractContextV3.factoryAbi),
-    UniswapContractContextV3.factoryAddress
-  );
+  private _uniswapFactoryContract =
+    this._ethersProvider.getContract<FactoryContractContext>(
+      JSON.stringify(UniswapContractContextV3.factoryAbi),
+      UniswapContractContextV3.factoryAddress
+    );
 
   constructor(private _ethersProvider: EthersProvider) {}
 
@@ -24,19 +25,5 @@ export class UniswapContractFactoryV3 {
     fee: BigNumberish
   ): Promise<string> {
     return await this._uniswapFactoryContract.getPool(token0, token1, fee);
-  }
-
-  public enableFeeAmount(fee: BigNumberish, tickSpacing: BigNumberish): string {
-    return this._uniswapFactoryContract.interface.encodeFunctionData(
-      'enableFeeAmount',
-      [fee, tickSpacing]
-    );
-  }
-
-  public feeAmountTickSpacing(parameter0: BigNumberish): string {
-    return this._uniswapFactoryContract.interface.encodeFunctionData(
-      'feeAmountTickSpacing',
-      [parameter0]
-    );
   }
 }
