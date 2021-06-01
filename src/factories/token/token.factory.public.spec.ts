cc64fdb03f3a520b4c76ab490f13db25aabb5bd4
Josh Stevens <joshstevens19@hotmail.co.uk>
2021-06-01 12:56:40 +0100

    token factory tests

--- src/factories/token/token.factory.public.spec.ts
+++ src/factories/token/token.factory.public.spec.ts
@@ -1,7 +1,9 @@
 import { ChainId, TokenFactoryPublic } from '../..';
-import { ContractContext } from '../../common/contract-context';
+import { UniswapVersion } from '../../enums/uniswap-version';
 import { MockEthereumAddress } from '../../mocks/ethereum-address.mock';
 import { MOCKFUN } from '../../mocks/fun-token.mock';
+import { UniswapContractContextV2 } from '../../uniswap-contract-context/uniswap-contract-context-v2';
+import { UniswapContractContextV3 } from '../../uniswap-contract-context/uniswap-contract-context-v3';
 
 describe('TokenFactoryPublic', () => {
   const token = MOCKFUN();
@@ -16,19 +18,52 @@ describe('TokenFactoryPublic', () => {
     expect(result).toEqual(token);
   });
 
-  it('allowance', async () => {
-    const result = await tokenFactoryPublic.allowance(MockEthereumAddress());
-    expect(result).not.toBeUndefined();
+  describe('allowance', () => {
+    describe('v2', () => {
+      it('allowance', async () => {
+        const result = await tokenFactoryPublic.allowance(
+          UniswapVersion.v2,
+          MockEthereumAddress()
+        );
+        expect(result).not.toBeUndefined();
+      });
+    });
+
+    describe('v3', () => {
+      it('allowance', async () => {
+        const result = await tokenFactoryPublic.allowance(
+          UniswapVersion.v3,
+          MockEthereumAddress()
+        );
+        expect(result).not.toBeUndefined();
+      });
+    });
   });
 
-  it('generateApproveAllowanceData', () => {
-    const result = tokenFactoryPublic.generateApproveAllowanceData(
-      ContractContext.routerAddress,
-      '0x05'
-    );
-    expect(result).toEqual(
-      '0x095ea7b30000000000000000000000007a250d5630b4cf539739df2c5dacb4c659f2488d0000000000000000000000000000000000000000000000000000000000000005'
-    );
+  describe('generateApproveAllowanceData', () => {
+    describe('v2', () => {
+      it('generateApproveAllowanceData', () => {
+        const result = tokenFactoryPublic.generateApproveAllowanceData(
+          UniswapContractContextV2.routerAddress,
+          '0x05'
+        );
+        expect(result).toEqual(
+          '0x095ea7b30000000000000000000000007a250d5630b4cf539739df2c5dacb4c659f2488d0000000000000000000000000000000000000000000000000000000000000005'
+        );
+      });
+    });
+
+    describe('v3', () => {
+      it('generateApproveAllowanceData', () => {
+        const result = tokenFactoryPublic.generateApproveAllowanceData(
+          UniswapContractContextV3.routerAddress,
+          '0x05'
+        );
+        expect(result).toEqual(
+          '0x095ea7b3000000000000000000000000e592427a0aece92de3edee1f18e0157c058615640000000000000000000000000000000000000000000000000000000000000005'
+        );
+      });
+    });
   });
 
   it('balanceOf', async () => {
@@ -41,13 +76,27 @@ describe('TokenFactoryPublic', () => {
     expect(result).toEqual('0x0f43f0ad89c30bb6');
   });
 
-  it('getAllowanceAndBalanceOf', async () => {
-    const result = await tokenFactoryPublic.getAllowanceAndBalanceOf(
-      MockEthereumAddress()
-    );
-    expect(result).toEqual({
-      allowance: '0x2386f01852b720',
-      balanceOf: '0x4d3f3832f7',
+  describe('v2', () => {
+    it('getAllowanceAndBalanceOf', async () => {
+      const result = await tokenFactoryPublic.getAllowanceAndBalanceOf(
+        UniswapVersion.v2,
+        MockEthereumAddress()
+      );
+      expect(result).toEqual({
+        allowance: '0x2386f01852b720',
+        balanceOf: '0x4d3f3832f7',
+      });
+    });
+
+    it('getAllowanceAndBalanceOf', async () => {
+      const result = await tokenFactoryPublic.getAllowanceAndBalanceOf(
+        UniswapVersion.v3,
+        MockEthereumAddress()
+      );
+      expect(result).toEqual({
+        allowance: '0x00',
+        balanceOf: '0x4d3f3832f7',
+      });
     });
   });
 });
