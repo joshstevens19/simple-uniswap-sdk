5ce261c1ac6edf5347c357d5f8a0212ebddc7e8f
liu <mailto123ads@gmail.com>
2022-06-30 02:19:36 +0800

    formatting

--- src/factories/router/models/route-methods.ts
+++ src/factories/router/models/route-methods.ts
@@ -1,62 +1,62 @@
 
 export type IRouterMethodName =
-| 'WETH'
-| 'addLiquidity'
-| 'addLiquidityETH'
-| 'factory'
-| 'getAmountIn'
-| 'getAmountOut'
-| 'getAmountsIn'
-| 'getAmountsOut'
-| 'quote'
-| 'removeLiquidity'
-| 'removeLiquidityETH'
-| 'removeLiquidityETHSupportingFeeOnTransferTokens'
-| 'removeLiquidityETHWithPermit'
-| 'removeLiquidityETHWithPermitSupportingFeeOnTransferTokens'
-| 'removeLiquidityWithPermit'
-| 'swapETHForExactTokens'
-| 'swapExactETHForTokens'
-| 'swapExactETHForTokensSupportingFeeOnTransferTokens'
-| 'swapExactTokensForETH'
-| 'swapExactTokensForETHSupportingFeeOnTransferTokens'
-| 'swapExactTokensForTokens'
-| 'swapExactTokensForTokensSupportingFeeOnTransferTokens'
-| 'swapTokensForExactETH'
-| 'swapTokensForExactTokens';
+  | 'WETH'
+  | 'addLiquidity'
+  | 'addLiquidityETH'
+  | 'factory'
+  | 'getAmountIn'
+  | 'getAmountOut'
+  | 'getAmountsIn'
+  | 'getAmountsOut'
+  | 'quote'
+  | 'removeLiquidity'
+  | 'removeLiquidityETH'
+  | 'removeLiquidityETHSupportingFeeOnTransferTokens'
+  | 'removeLiquidityETHWithPermit'
+  | 'removeLiquidityETHWithPermitSupportingFeeOnTransferTokens'
+  | 'removeLiquidityWithPermit'
+  | 'swapETHForExactTokens'
+  | 'swapExactETHForTokens'
+  | 'swapExactETHForTokensSupportingFeeOnTransferTokens'
+  | 'swapExactTokensForETH'
+  | 'swapExactTokensForETHSupportingFeeOnTransferTokens'
+  | 'swapExactTokensForTokens'
+  | 'swapExactTokensForTokensSupportingFeeOnTransferTokens'
+  | 'swapTokensForExactETH'
+  | 'swapTokensForExactTokens';
 
 export type IRouterMethods = {
-[key in IRouterMethodName]: string;
+  [key in IRouterMethodName]: string;
 };
 
 export const DEFAULT_ROUTER_METHOD: IRouterMethods = {
-WETH: 'WETH',
-addLiquidity: 'addLiquidity',
-addLiquidityETH: 'addLiquidityETH',
-factory: 'factory',
-getAmountIn: 'getAmountIn',
-getAmountOut: 'getAmountOut',
-getAmountsIn: 'getAmountsIn',
-getAmountsOut: 'getAmountsOut',
-quote: 'quote',
-removeLiquidity: 'removeLiquidity',
-removeLiquidityETH: 'removeLiquidityETH',
-removeLiquidityETHSupportingFeeOnTransferTokens:
-  'removeLiquidityETHSupportingFeeOnTransferTokens',
-removeLiquidityETHWithPermit: 'removeLiquidityETHWithPermit',
-removeLiquidityETHWithPermitSupportingFeeOnTransferTokens:
-  'removeLiquidityETHWithPermitSupportingFeeOnTransferTokens',
-removeLiquidityWithPermit: 'removeLiquidityWithPermit',
-swapETHForExactTokens: 'swapETHForExactTokens',
-swapExactETHForTokens: 'swapExactETHForTokens',
-swapExactETHForTokensSupportingFeeOnTransferTokens:
-  'swapExactETHForTokensSupportingFeeOnTransferTokens',
-swapExactTokensForETH: 'swapExactTokensForETH',
-swapExactTokensForETHSupportingFeeOnTransferTokens:
-  'swapExactTokensForETHSupportingFeeOnTransferTokens',
-swapExactTokensForTokens: 'swapExactTokensForTokens',
-swapExactTokensForTokensSupportingFeeOnTransferTokens:
-  'swapExactTokensForTokensSupportingFeeOnTransferTokens',
-swapTokensForExactETH: 'swapTokensForExactETH',
-swapTokensForExactTokens: 'swapTokensForExactTokens',
+  WETH: 'WETH',
+  addLiquidity: 'addLiquidity',
+  addLiquidityETH: 'addLiquidityETH',
+  factory: 'factory',
+  getAmountIn: 'getAmountIn',
+  getAmountOut: 'getAmountOut',
+  getAmountsIn: 'getAmountsIn',
+  getAmountsOut: 'getAmountsOut',
+  quote: 'quote',
+  removeLiquidity: 'removeLiquidity',
+  removeLiquidityETH: 'removeLiquidityETH',
+  removeLiquidityETHSupportingFeeOnTransferTokens:
+    'removeLiquidityETHSupportingFeeOnTransferTokens',
+  removeLiquidityETHWithPermit: 'removeLiquidityETHWithPermit',
+  removeLiquidityETHWithPermitSupportingFeeOnTransferTokens:
+    'removeLiquidityETHWithPermitSupportingFeeOnTransferTokens',
+  removeLiquidityWithPermit: 'removeLiquidityWithPermit',
+  swapETHForExactTokens: 'swapETHForExactTokens',
+  swapExactETHForTokens: 'swapExactETHForTokens',
+  swapExactETHForTokensSupportingFeeOnTransferTokens:
+    'swapExactETHForTokensSupportingFeeOnTransferTokens',
+  swapExactTokensForETH: 'swapExactTokensForETH',
+  swapExactTokensForETHSupportingFeeOnTransferTokens:
+    'swapExactTokensForETHSupportingFeeOnTransferTokens',
+  swapExactTokensForTokens: 'swapExactTokensForTokens',
+  swapExactTokensForTokensSupportingFeeOnTransferTokens:
+    'swapExactTokensForTokensSupportingFeeOnTransferTokens',
+  swapTokensForExactETH: 'swapTokensForExactETH',
+  swapTokensForExactTokens: 'swapTokensForExactTokens',
 };
